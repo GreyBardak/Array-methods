c26445cfd4063b4567896c334d1aaaedd7a7df85
GreyBardak <35662429+GreyBardak@users.noreply.github.com>
2018-11-12 23:43:00 +0200

    Add files via upload

---
+++ 1script.js
@@ -0,0 +1,9 @@
+var myArray = ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+myArray.push(-33.5);
+console.log(myArray);
+myArray.unshift(1000);
+console.log(myArray);
+myArray.push("Last array element");
+console.log(myArray);
+myArray.splice(3, 1);
+console.log(myArray);
